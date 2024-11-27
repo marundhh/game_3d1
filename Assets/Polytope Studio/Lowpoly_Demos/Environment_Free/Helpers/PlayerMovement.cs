f2b6798a0d61dec8084a753a0ac383d62766934f
trideptraivcl <tridmps38521@gmail.com>
2024-11-28 00:03:15 +0700

    lo

--- Assets/Polytope Studio/Lowpoly_Demos/Environment_Free/Helpers/PlayerMovement.cs
+++ Assets/Polytope Studio/Lowpoly_Demos/Environment_Free/Helpers/PlayerMovement.cs
@@ -1,53 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class SimplePlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    public float speed = 5f; // Tốc độ di chuyển
 
-    public float speed = 5;
-    public float gravity = -9.18f;
-    public float jumpHeight = 3f;
-
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-
-    Vector3 velocity;
-    bool isGrounded;
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        // Nhận input từ bàn phím
+        float moveX = Input.GetAxis("Horizontal"); // A/D hoặc mũi tên trái/phải
+        float moveZ = Input.GetAxis("Vertical");   // W/S hoặc mũi tên lên/xuống
 
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
+        // Tạo vector di chuyển
+        Vector3 move = new Vector3(moveX, 0, moveZ);
 
-        if (Input.GetKey("left shift") && isGrounded)
-        {
-            speed = 10;
-        }
-        else
-        {
-            speed = 5;
-        }
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
+        // Di chuyển nhân vật
+        transform.Translate(move * speed * Time.deltaTime, Space.World);
     }
 }
